a7c86553f063a69cc4c142bdd2c90a74332002c3
Jsav-bc <jordansavagek@gmail.com>
2025-05-08 10:50:17 -0500

    Image Saving Added and Class

--- Agents/ppo_agent.py
+++ Agents/ppo_agent.py
@@ -1,4 +1,9 @@
 import retro
+import imageio
+import numpy as np
+import os
+import imageio
+import random
 from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import(
     SubprocVecEnv,
@@ -9,20 +14,22 @@ from Wrappers import NoProgressPenaltyWrapper
 
 def make_env():
     def _init():
-        env = retro.make(game='SuperMarioWorld-Snes',state='YoshiIsland2',scenario='SMW')
-        env = NoProgressPenaltyWrapper(env,timeout=2500)
+        #env = retro.make(game='SuperMarioWorld-Snes',state='YoshiIsland2',scenario='SMW')
+        env = retro.make(game='Airstriker-Genesis',render_mode='rgb_array')
+        #env = NoProgressPenaltyWrapper(env,timeout=2500)
         return env
     return _init
 def main():
-    venv = VecTransposeImage(VecFrameStack(SubprocVecEnv([make_env()] * 12), n_stack=6))
+    #TODO Receive Parent ID from DB 
+    venv = VecTransposeImage(VecFrameStack(SubprocVecEnv([make_env()] * 1), n_stack=4))
 
     model = PPO(
             policy="CnnPolicy",
             env=venv,
-            learning_rate=lambda f: f * 2.5e-1,
+            learning_rate=lambda f: f * 2.5e-3,
             n_steps=128,
-            batch_size=64,
-            n_epochs=10,
+            batch_size=32,
+            n_epochs=2,
             gamma=0.99,
             gae_lambda=0.95,
             clip_range=0.1,
@@ -30,17 +37,21 @@ def main():
             verbose=1,
         )
 
-    model.learn(total_timesteps = 4000000)
-    model.save("ppo_smbtwo")
+    model.learn(total_timesteps = 1280)
+    model.save("ppo_as")
 
+    images = []
     obs = venv.reset()
+    img = model.env.render(mode="rgb_array")
+    event_id = random.random()
+    os.mkdir(f"/home/jordan/boot/snesai/CV_SNES/Games_States/{event_id}")
+    
+    for i in range(350):
+        images.append(img)
+        action,_ = model.predict(obs)
+        obs,_,_,_ = model.env.step(action)
+        img = model.env.render(mode="rgb_array")
+        imageio.imwrite(f"/home/jordan/boot/snesai/CV_SNES/Games_States/{event_id}/{i}_test.png",img)
 
 if __name__ == "__main__":
     main()
-'''
-while True: 
-    action, _states = model.predict(obs)
-    obs, rewards, dones, info = venv.step(action)
-    print(obs,rewards,dones,info)
-    venv.render()
-'''
---
+++ Classifier/frame_stacker.py
@@ -0,0 +1,7 @@
+import os
+import imageio
+
+frames = os.listdir("")
+print(frames)
+im = imageio.imread("")
+print(im.shape)
