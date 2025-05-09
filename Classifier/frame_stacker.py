49b07e103651579b2e0677f562d262d0ad7f89b6
Jsav-bc <jordansavagek@gmail.com>
2025-05-09 16:07:26 -0500

    Funcd Frame_Stack

--- Classifier/frame_stacker.py
+++ Classifier/frame_stacker.py
@@ -1,47 +1,48 @@
 import os
-import numpy as np
 import imageio
-import torch
-import torchvision
-import torchvision.transforms
-import cv2 
+
 
 ## COULD RESTRUCT INTO A CLASS TO PULL SAMPLES AT RUNTIME
-folder_path = "xxxxxxxx"
-frame_stack_len = 6
-p_fold = folder_path.split('/')[-1]
-ifiles = []
+def framestacker(folder_path,skip_frame = 3,frame_stack_len = 3):
+    folder_path = folder_path
+    skip_frame = skip_frame
+    frame_stack_len = frame_stack_len
+    p_fold = folder_path.split('/')[-1]
+    ifiles = []
 
-for f in os.listdir(folder_path):
-    i_file = {
-    "fpath": (f"{folder_path}/{f}"),
-    "sequ": int(f.rsplit('/')[-1].split('_')[0])
-    }
-    ifiles.append(i_file)
-image_files = sorted(ifiles,key=lambda x: x['sequ'])
+    for f in os.listdir(folder_path):
+        i_file = {
+        "fpath": (f"{folder_path}/{f}"),
+        "sequ": int(f.rsplit('/')[-1].split('_')[0])
+        }
+        ifiles.append(i_file)
+    image_files = sorted(ifiles,key=lambda x: x['sequ'])
 
-framestacks_ls = []
-framestacks = []
+    framestacks_ls = []
+    framestacks = []
 
-for i in image_files:
-    f_stack = []
-    base_index = i['sequ']
-    m = len(image_files)
-    for j in range(frame_stack_len):
-        frame_index = base_index + j*3
-        if frame_index >= m :
-            f_stack.append(m-1)
-        else:
-            f_stack.append(frame_index)
-    framestacks.append(f_stack)
+    for i in image_files:
+        f_stack = []
+        base_index = i['sequ']
+        m = len(image_files)
+        for j in range(frame_stack_len):
+            frame_index = base_index + j*skip_frame
+            if frame_index >= m :
+                f_stack.append(m-1)
+            else:
+                f_stack.append(frame_index)
+        framestacks.append(f_stack)
+        
+
+    for framestack in framestacks:
+        framestacks_lib = {
+        'event': p_fold,
+        'run_id': framestack,
+        'frame_seq': [image_files[i]['fpath'] for i in framestack],
+        'label': None,
+        'Train': False,
+        'Verified': False
+        }
+        framestacks_ls.append(framestacks_lib)
     
-
-for framestack in framestacks:
-    framestacks_lib = {
-    'event': p_fold,
-    'run_id': framestack,
-    'frame_seq': [image_files[i]['fpath'] for i in framestack]
-    }
-    framestacks_ls.append(framestacks_lib)
-
-print(framestacks_ls)
+    return framestacks_ls
---
+++ Classifier/manual_labeler.py
@@ -0,0 +1,4 @@
+import frame_stacker
+
+ls = frame_stacker.framestacker("/home/jordan/boot/snesai/CV_SNES/Games_States/0.5222159894470562", skip_frame=3,frame_stack_len=4)
+print(ls)
