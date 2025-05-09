d0e61c326444895c4f71bfa8b9fe3d103e84ea2c
Jsav-bc <jordansavagek@gmail.com>
2025-05-09 14:46:04 -0500

    Stack dev

--- Classifier/frame_stacker.py
+++ Classifier/frame_stacker.py
@@ -6,17 +6,35 @@ import torchvision.transforms
 import cv2 
 
 folder_path = "/home/jordan/boot/snesai/CV_SNES/Games_States/0.5222159894470562"
-frame_stack_len = 4
+frame_stack_len = 6
 
-image_files = sorted([os.path.join(folder_path, f) for f in os.listdir(folder_path)])
+ifiles = []
 
-framstacks = []
+for f in os.listdir(folder_path):
+    i_file = {
+    "fpath": (f"{folder_path}/{f}"),
+    "sequ": int(f.rsplit('/')[-1].split('_')[0])
+    }
+    ifiles.append(i_file)
+image_files = sorted(ifiles,key=lambda x: x['sequ'])
+
+framestacks_lib = {
+    'run_id': '',
+    'frame_seq': []
+}
+framestacks = []
 
 for i in image_files:
     f_stack = []
+    base_index = i['sequ']
+    m = len(image_files)
     for j in range(frame_stack_len):
-        f_stack.append(image_files[j])
-    framstacks.append(f_stack)
-    j+=1
+        frame_index = base_index + j*3
+        if frame_index >= m :
+            f_stack.append(m-1)
+        else:
+            f_stack.append(frame_index)
+    framestacks.append(f_stack)
+    
 
-print(framstacks)
+print(framestacks)
